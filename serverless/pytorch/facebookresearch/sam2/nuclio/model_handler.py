bdc4fd9063e5d9179fd1ff092aef9ac811b2ea52
Gundars Kulikovskis <gundars.kulikovskis@zippyvision.com>
2025-08-18 21:54:25 +0300

    Added sam2 initially

--- serverless/pytorch/facebookresearch/sam2/nuclio/model_handler.py
+++ serverless/pytorch/facebookresearch/sam2/nuclio/model_handler.py
@@ -1,22 +1,32 @@
-# Copyright (C) CVAT.ai Corporation
-#
-# SPDX-License-Identifier: MIT
+# model_handler.py  (SAM2 version)
 
+import os
 import numpy as np
 import torch
-from segment_anything import sam_model_registry, SamPredictor
+
+from sam2.build_sam import build_sam2
+from sam2.sam2_image_predictor import SAM2ImagePredictor
 
 class ModelHandler:
     def __init__(self):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.sam_checkpoint = "/opt/nuclio/sam/sam_vit_h_4b8939.pth"
-        self.model_type = "vit_h"
-        self.latest_image = None
-        sam_model = sam_model_registry[self.model_type](checkpoint=self.sam_checkpoint)
-        sam_model.to(device=self.device)
-        self.predictor = SamPredictor(sam_model)
+
+        # Use env vars so you can control them from function.yaml
+        self.sam2_checkpoint = os.environ.get(
+            "SAM2_CHECKPOINT",
+            "/opt/nuclio/sam2.1_hiera_large.pt",
+        )
+        self.sam2_config = os.environ.get(
+            "SAM2_CONFIG",
+            "/opt/sam2/configs/sam2.1/sam2.1_hiera_l.yaml",
+        )
+
+        # Build SAM2 and predictor
+        sam2_model = build_sam2(self.sam2_config, self.sam2_checkpoint, device=self.device)
+        self.predictor = SAM2ImagePredictor(sam2_model)
 
     def handle(self, image):
+        # Same flow as SAM v1: set image, get image embedding
         self.predictor.set_image(np.array(image))
         features = self.predictor.get_image_embedding()
         return features
